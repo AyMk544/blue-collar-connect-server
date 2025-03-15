6eeb14f22e427958b4d73de643d7d77a61b38114
anubhab-nath098 <anubhabnath19@gmail.com>
2025-03-15 12:55:38 +0530

    community API

--- src/routes/community-routes.ts
+++ src/routes/community-routes.ts
@@ -1,8 +1,20 @@
 import { Hono } from 'hono' 
-import { authMiddleware } from '../middlewares/authMiddleware.js'
+import { authMiddleware,workerMiddleware } from '../middlewares/authMiddleware.js'
+import { createCommunityPost, createWorkerCommunity, getAllCommunities, getCommunityById, getCommunityPosts, getUserCommunities, getUserJoinedCommunityPosts, joinWorkerCommunity, leaveWorkerCommunity, searchCommunityByName } from '../controllers/communitiesController.js'
 const communityRoutes = new Hono()
 
 communityRoutes.use('*', authMiddleware)
+communityRoutes.post("/create-community", createWorkerCommunity)
+communityRoutes.post("/join", joinWorkerCommunity)
+communityRoutes.post("/leave",leaveWorkerCommunity)
+communityRoutes.get("/joined/:userId", getUserCommunities)
+communityRoutes.get("/search", searchCommunityByName)
+communityRoutes.get("/all", getAllCommunities)
+communityRoutes.post("/posts",createCommunityPost)
+communityRoutes.get("/posts",getCommunityPosts)
+communityRoutes.get('/joined-posts', getUserJoinedCommunityPosts);
+communityRoutes.get("/:id", getCommunityById)
+
 
 
 export default communityRoutes
