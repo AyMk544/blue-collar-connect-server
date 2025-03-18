0caecdac9e4d8543a3ee0337a95655b7881e602d
anubhab-nath098 <anubhabnath19@gmail.com>
2025-03-18 22:58:19 +0530

    added middlewares properly

--- src/routes/job-routes.ts
+++ src/routes/job-routes.ts
@@ -5,7 +5,7 @@ import { createJobPost, fetchJobPosts } from '../controllers/jobController.js'
 const jobRoutes = new Hono()
 
 jobRoutes.use('*', authMiddleware)
-jobRoutes.post('/create', createJobPost, employerMiddleware)
-jobRoutes.get('/all',fetchJobPosts,workerMiddleware)
+jobRoutes.post('/create', employerMiddleware, createJobPost)
+jobRoutes.get('/all',workerMiddleware,fetchJobPosts)
 
 export default jobRoutes
